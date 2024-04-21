10c3a28b3538f8cb43b98b15e56adc548b74cc2b
ahilleas08 <aif00002@mix.wvu.edu>
2024-04-21 12:37:46 -0400

    Lab 7 Makeup Submission
    Makeup Submission for lab 7

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
-import Card from './Card';
 
 
 function App() {
   return (
     <div className="App">
       <div>
-      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800361700</p>
       <p>Hi I am Ahilleas</p>
     </div>
-    <Card />
     </div>
   );
 }
