5bcc1e96d2a9604e592423d8f22706183d64ecf1
ahilleas08 <aif00002@mix.wvu.edu>
2024-04-21 12:40:32 -0400

    Lab 7 Makeup
    Lab 7 Makeup fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function App() {
   return (
     <div className="App">
       <div>
+
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800361700</p>
       <p>Hi I am Ahilleas</p>
     </div>
+
     </div>
   );
 }
